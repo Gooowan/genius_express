a5d7302ae6ee9bfaa141576a6b708466502e0f9f
BUNJEE <vsolomatin@kse.org.ua>
2024-02-29 03:15:31 +0200

    error in validation form during log in

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -2,41 +2,27 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const axios = require('axios');
+const config = require('../config');
 
 exports.signup = (req, res) => {
     axios.get('http://127.0.0.1:8000/signup/', req.body)
         .then(response => {
             res.send(response.data);
-
-            // if (!response.data.password) {
-            //     return res.status(400).send({ message: 'Password is required' });
-            // }
-            //
-            // const user = new User({
-            //     fullName: response.data.username,
-            //     role: response.data.role,
-            //     password: bcrypt.hashSync(response.data.password, 8)
-            // });
-            //
-            // user.save((err, user) => {
-            //     if (err) {
-            //         res.status(500)
-            //             .send({
-            //                 message: err
-            //             });
-            //         return;
-            //     } else {
-            //         res.status(200)
-            //             .send({
-            //                 message: "User Registered successfully"
-            //             })
-            //     }
-            // })
         })
         .catch(error => {
             console.error(error);
         });
 };
+
+exports.login = (req, res) => {
+    axios.get('http://127.0.0.1:8000/login/', req.body)
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
 
 exports.saveuser = (req, res) => {
     const { username, role, password } = req.body;
@@ -58,51 +44,31 @@ exports.saveuser = (req, res) => {
 
 };
 
-exports.login = (req, res) => {
-    User.findOne({
-        email: req.body.email
-    })
-        .exec((err, user) => {
-            if (err) {
-                res.status(500)
-                    .send({
-                        message: err
-                    });
-                return;
-            }
-            if (!user) {
-                return res.status(404)
-                    .send({
-                        message: "User Not found."
-                    });
-            }
+exports.loginuser = async (req, res) => {
+    const { username, password } = req.body;
 
-            const passwordIsValid = bcrypt.compareSync(
-                req.body.password,
-                user.password
-            );
-            if (!passwordIsValid) {
-                return res.status(401)
-                    .send({
-                        accessToken: null,
-                        message: "Invalid Password!"
-                    });
-            }
-            const token = jwt.sign({
-                id: user.id
-            }, process.env.API_SECRET, {
-                expiresIn: 86400
-            });
+    const user = await User.findOne({ username: username }).exec();
+    if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+    }
 
-            res.status(200)
-                .send({
-                    user: {
-                        id: user._id,
-                        email: user.email,
-                        fullName: user.fullName,
-                    },
-                    message: "Login successfull",
-                    accessToken: token,
-                });
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+    if (!passwordIsValid) {
+        return res.status(401).send({
+            accessToken: null,
+            message: "Invalid Password!"
         });
+    }
+
+    const token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 86400
+    })
+        .then(() => {
+            res.status(200).send({ message: "User login successfully" });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
+
 };
--- app/routes/songRoutes.js
+++ app/routes/songRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { getSongs, getSong, createSong, updateSong, deleteSong, getEditForm } = require('../controllers/songController');
-const {login, signup, saveuser} = require("../controllers/authController");
+const {login, signup, saveuser, loginuser} = require("../controllers/authController");
 
 const router = express.Router();
 
 router.get('/', getSongs);
 router.get('/new', (req, res) => res.render('addSong'));
+router.post('/saveuser', saveuser);
+router.post('/loginuser', loginuser);
 router.post('/new', createSong);
 router.get('/login', login);
 router.get('/signup', signup);
-router.post('/saveuser', saveuser);
 router.get('/:id', getSong);
 router.put('/:id', updateSong);
 router.delete('/:id', deleteSong);
